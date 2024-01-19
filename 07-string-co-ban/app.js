35a252491a5b2b25a2dfb01de9b60e412d624ca9
TranDangKhoi <baits16@gmail.com>
2024-01-19 22:24:02 +0700

    :100: Update lectures

---
+++ 07-string-co-ban/app.js
@@ -0,0 +1,9 @@
+// double quotes, single quote, backticks (template literals/template string)
+// "", '', ``
+
+const currentJob = "Front-end Developer";
+const myName = "Tran Dang Khoi";
+// I'm Tran Dang Khoi and I'm a Front-end Developer
+// const newString = "I'm " + myName + " and I'm a " + currentJob;
+const newString2 = `I'm ${myName} and I'm a ${currentJob}`;
+console.log(newString2);
