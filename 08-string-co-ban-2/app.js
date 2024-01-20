bfa80b4afe276087e1840091bc7695bcdeeb660b
TranDangKhoi <baits16@gmail.com>
2024-01-20 23:13:17 +0700

    :+1: Update lectures

---
+++ 08-string-co-ban-2/app.js
@@ -0,0 +1,15 @@
+const myRandomString = "        ABCXYZ          ";
+
+// console.log("chatAt", myRandomString.charAt(0));
+// console.log("concat", myRandomString.concat("JKL"));
+// console.log("endsWith", myRandomString.endsWith("B"));
+// console.log("includes", myRandomString.includes("ABZ"));
+// so sánh: "cát" và "cat"
+// console.log("repeat", myRandomString.repeat(4));
+// console.log("replace", myRandomString.replace("XYZ", "JKL"));
+// console.log("search", myRandomString.search("XYZ"));
+// console.log("slice", myRandomString.slice(1, 4));
+// Chưa trim():
+// console.log(myRandomString.length);
+// Sau khi trim():
+// console.log(myRandomString.trim().length);
