e34ac1070b16ce6ebc4cc3231138529079d64a83
TranDangKhoi <baits16@gmail.com>
2024-01-21 18:04:57 +0700

    :+1: Update course

---
+++ 11-number-co-ban/app.js
@@ -0,0 +1,10 @@
+const num1 = 999.8382499;
+// console.log(num1.toFixed(3));
+// 2.000.000 -> 2M/2Tr
+// console.log(num1.toLocaleString("en-GB", {}));
+// console.log(num1.toString());
+// Math
+// const newNum = Math.pow(9, 2);
+// console.log(newNum);
+// Math.round/ceil/pow/min/max/random
+// const minNum = Math.ceil(2.1);
